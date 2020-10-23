67377071079b9c62aec35feab178e58b67e3e92e
Rizky Ramadhan <ramadhanky68@gmail.com>
2020-10-23 09:24:38 +0700

    insert data

--- ci4app/app/Views/komik/index.php
+++ ci4app/app/Views/komik/index.php
@@ -3,7 +3,13 @@
 <div class="container">
     <div class="row">
         <div class="col">
+            <a href="/komik/create" class="btn btn-primary mt-3">Tambah data komik</a>
             <h1 class="mt-2">Daftar Komik</h1>
+            <?php if (session()->getFlashdata('pesan')) :  ?>
+                <div class="alert alert-success" role="alert">
+                    <?= session()->getFlashdata('pesan'); ?>
+                </div>
+            <?php endif; ?>
             <table class="table">
                 <thead>
                     <tr>
